1baf530f7abe254e019b779392a2328ad9de6df9
tinnah13 <a.umuhoza2@alustudent.com>
2026-01-29 05:38:46 -0800

    just done with analayze logs file

---
+++ analyze_logs.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+
+                                # part 1: user interactive Prompt
+			  #-------------------------------------------	
+
+
+
+
+# creating variables for file paths
+
+HEART_RATE_LOG="hospital_data/active_logs/heart_rate_log.log"
+TEMPERATURE_LOG="hospital_data/active_logs/temperature_log.log"
+WATER_USAGE_LOG="hospital_data/active_logs/water_usage_log.log"
+ANALYSIS_REPORT="hospital_data/reports/analysis_report.txt"
+
+
+# this function is aim to display the menu, but it don't really do anything(just to display choice)
+
+display_menu() {
+    echo ""
+    echo "Select log file to analyze:"
+    echo "1) Heart Rate "
+    echo "2) Temperature "
+    echo "3) Water Usage "
+    echo -n "Enter choice (1-3): "
+  
+}
+
+
+# here is a function that will let user select one the displayed choice
+
+while true; do
+    display_menu
+    read choice
+
+    case $choice in
+        1)
+            LOG_FILES=$HEART_RATE_LOG
+            LOG_NAME="Heart Rate"
+            break
+            ;;
+        2)
+            LOG_FILES=$TEMPERATURE_LOG
+            LOG_NAME="Temperature"
+            break
+            ;;
+        3)
+            LOG_FILES=$WATER_USAGE_LOG
+            LOG_NAME="Water Usage"
+            break
+            ;;
+        *)
+            echo "Invalid choice. Please enter a number between 1 and 3."
+            ;;
+    esac
+done
+
+echo ""
+
+
+                                       # part 2: anlysis functionality
+				    #-----------------------------------
+   
+
+
+echo ""
+echo "Analyzing $LOG_NAME log file: $LOG_FILES"
+
+
+#checking if we arleady have $ANALYSIS REPORT file. if not exist, then create it
+
+if [ ! -f "$ANALYSIS_REPORT" ]; then
+	touch hospital_data/reports/analysis_report.txt
+fi
+
+# from this part, everything we do, will be saved into $ANALYSIS REPORT file(including "echo" messages)
+
+# this code block is for checking if the log file exists before proceeding other commands
+if [ ! -f "$LOG_FILES" ]; then
+    echo "Error: Log file '$LOG_FILES' not found." | tee -a "$ANALYSIS_REPORT"
+    exit 1
+fi
+{
+# if file exist, then we print echo for the user that analysis proccess has started
+echo ""
+echo ""
+echo "--- Analysis Report for $LOG_NAME ($LOG_FILES) ---"
+echo ""
+echo "Generated on: "$(date +'%Y-%m-%d_%H:%M:%S')""
+echo ""
+# Use awk to process the log file
+# It extracts the device, counts occurrences, and finds first/last timestamps.
+
+echo "---------- $LOG_NAME analysis -------" 
+echo ""
+echo ""
+echo "Counts of accurrances for each Device"
+echo "-------------------------------------"
+echo ""
+awk '{print $3}' "$LOG_FILES" | sort | uniq -c
+echo ""
+echo ""
+echo "Fist recorded entry:"
+head -n 1 $LOG_FILES
+echo ""
+echo ""
+echo "Last recorded entry:"
+tail -n 1 $LOG_FILES
+echo ""
+echo ""
+echo "--- End of Report ---"
+echo ""
+echo ""
+echo ""
+echo ""
+} | tee -a "$ANALYSIS_REPORT"
